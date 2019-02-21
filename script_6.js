cd13c8ab04ea942fc43079a2eb619f518dda10ce
Paul.Graton <paulgtn@hotmail.fr>
2019-02-21 19:36:32 +0100

    All Done mais y'a forcément plus simple

---
+++ script_6.js
@@ -0,0 +1,40 @@
+const entrepreneurs = [
+    { first: 'Steve', last: 'Jobs', year: 1955 },
+    { first: 'Bill', last: 'Gates', year: 1955 },
+    { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+    { first: 'Jeff', last: 'Bezos', year: 1964 },
+    { first: 'Elon', last: 'Musk', year: 1971 },
+    { first: 'Larry', last: 'Page', year: 1973 },
+    { first: 'Jack', last: 'Dorsey', year: 1976 },
+    { first: 'Evan', last: 'Spiegel', year: 1990 },
+    { first: 'Brian', last: 'Chesky', year: 1981 },
+    { first: 'Travis', last: 'Kalanick', year: 1976 },
+    { first: 'Marc', last: 'Andreessen', year: 1971 },
+    { first: 'Peter', last: 'Thiel', year: 1967 }
+];
+
+let bornInThe70 = [];
+entrepreneurs.forEach(e => {
+    if (e.year < 1980 && e.year >= 1970) {
+        bornInThe70.push(e)
+    }
+})
+console.log(bornInThe70);
+
+let name = [];
+let x = 0;
+entrepreneurs.forEach(e => {
+    name.push(e.first + " " + e.last)
+})
+console.log(name);
+
+let age = []
+entrepreneurs.map((e, i) => {
+    age[i] = 2019 - e.year;
+})
+entrepreneurs.map((e, i) => {
+    console.log(e.last + ":" + age[i])
+});
+
+let lastOrder = entrepreneurs.sort(entrepreneurs.last)
+console.log(lastOrder)
---
+++ script_7.js
@@ -0,0 +1,62 @@
+
+const books = [
+    { title: 'Gatsby le magnifique', id: 133712, rented: 9 },
+    { title: 'A la recherche du temps,perdu', id: 237634, rented: 2 },
+    { title: 'Orgueil & Préjugés', id: 873495, rented: 6 },
+    { title: 'Les frères Karamazov', id: 450911, rented: 5 },
+    { title: 'Guerre et Paix', id: 748147, rented: 19 }
+];
+
+let n = 0;
+books.forEach(book => {
+    if (book.rented !== 0) {
+        n++;
+    }
+})
+console.log(n === books.length);
+console.log(books.length)
+
+let maxRented;
+let maxBook = 0;
+books.forEach(book => {
+    if (book.rented > maxBook) {
+        maxBook = book.rented;
+        maxRented = book;
+    }
+})
+console.log(`Le livre le plus emprunté est : ${maxRented.title}`)
+
+let minRented;
+let minBook = Infinity;
+books.forEach(book => {
+    if (book.rented < minBook) {
+        minBook = book.rented;
+        minRented = book;
+    }
+})
+console.log(`Le livre le moins emprunté est : ${minRented.title}`)
+
+let searchBook;
+books.forEach(book => {
+    if (book.id === 873495) {
+        searchBook = book;
+    }
+})
+console.log(`Le livre avec l'id 873495 est : ${searchBook.title}`);
+
+let index;
+books.forEach((book, i) => {
+    if (book.id === 133712) {
+        index = i;
+    }
+})
+books.splice(index, 1);
+console.log(books);
+
+let bookOrder = books.sort(books.title);
+console.log(bookOrder);
+
+/*
+let search = books.find(id)
+console.log(`Le livre avec l'id 873495 est : ${search.title}`);
+*/
